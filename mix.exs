3b05e953dfa98fa3a383f01362ba5ad8802cb3e4
Marc Nickert <git@marcnickert.de>
2026-07-24 23:57:20 +0200

    Point package source_url at the marcnnn/eexcharts repo

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule EexCharts.MixProject do
   use Mix.Project
 
   @version "0.1.0"
-  @source_url "https://github.com/marcnickert/eexcharts"
+  @source_url "https://github.com/marcnnn/eexcharts"
 
   def project do
     [
